ed27ccec550cd33646f4f69583bbf892467d4ba7
Waleed <wo2111181@gmail.com>
2025-01-29 18:27:47 +0200

    implement home page

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -2,6 +2,6 @@
 import "./CustomButton.css"
 export function CustomButton({ text, className, onClick }) {
     return (
-        <button className={className} onClick={onClick}>{text}</button>
+        <button  className={className} onClick={onClick}>{text}</button>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,38 @@
-
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
 export function Home() {
     return (
-        <h1>home page</h1>
-    )
+        <Container className="container mt-5">
+            <Row>
+                <Col md={12}>
+                    <Card className="text-center">
+                        <Card.Body>
+                            <h1>Welcome to the Cart App!</h1>
+                            <p>This is a basic cart app project built using React and Redux Toolkit.</p>
+                            <p>The app is based on a course that covers the main concepts of React.</p>
+                            <h4>Course Playlist:</h4>
+                            <Button
+                                href="https://youtube.com/playlist?list=PLQtNtS-WfRa9LbmD8ON7rWhn-AtKTGdkn"
+                                target="_blank"
+                                variant="primary">
+                                Watch on YouTube
+                            </Button>
+                            <br />
+                            <h4>Special Thanks to the Instructor:</h4>
+                            <p>
+                                A special thanks to <strong>Eng. Ahmed Naser</strong> for the excellent course.
+                            </p>
+                            <h4>Source Code :</h4>
+                            <Button
+                                href="https://github.com/owalid750/cart-app"
+                                target="_blank"
+                                variant="info">
+                                GitHub Repository
+                            </Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
